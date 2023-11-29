eeb9d8c61ac0bc51d4fa55ff1228adccde973c9f
Long Vu <Longvu2000@csu.fullerton.edu>
2023-11-29 13:52:50 -0800

    Working server and client, can q and send files

--- client.py
+++ client.py
@@ -3,13 +3,38 @@ import socket
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4500
 ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
 
 def main(): 
-    
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # AF_INET for IPV4, SOCK_STREAM for TCP connection type
     client.connect(ADDR)
-    
-    
+
+    # Get command from the user
+    command = input("Enter command (q for quit, f for file transfer): ")
+
+    # Send command to the server
+    client.send(command.encode(FORMAT))
+
+    if command == 'f':
+        file = open("data/test.txt", "r")
+        data = file.read()
+
+        # Send name of file
+        client.send("test.txt".encode(FORMAT))
+        msg = client.recv(SIZE).decode(FORMAT)
+
+        print(f"SERVER: {msg}")
+        
+        # Send the data itself
+        client.send(data.encode(FORMAT))
+        msg = client.recv(SIZE).decode(FORMAT)
+
+        print(f"SERVER: {msg}")
+
+        file.close()
+
+    client.close()
+
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -3,18 +3,49 @@ import socket
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4500
 ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
 
 def main(): 
     print("STARTING SERVER")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # AF_INET for IPV4, SOCK_STREAM for TCP connection type
     server.bind(ADDR)
     server.listen()
     print("LISTENING FOR CONNECTION")
 
-    while True:
+    running = True
+    while running:
         connection, address = server.accept()
         print(f"NEW CONNECTION {address} CONNECTED")
 
+        # Get command from the client
+        command = connection.recv(SIZE).decode(FORMAT)
+
+        if command == 'q':
+            print(f"CLIENT {address} disconnected")
+            connection.close()
+            running = False
+        elif command == 'f':
+            # Get file name from the client
+            filename = connection.recv(SIZE).decode(FORMAT)
+            print("RECEIVED File")
+
+            file = open(filename, "w")
+            connection.send("Filename Received". encode(FORMAT))
+
+            # Get file data from the client
+            data = connection.recv(SIZE).decode(FORMAT)
+            print("File data received")
+
+            # Write data into the file and send confirmation
+            file.write(data)
+            connection.send("File data Received".encode(FORMAT))
+
+            file.close()
+            connection.close()
+            print(f"CLIENT {address} disconnected")
+
+    print("SERVER SHUTTING DOWN")
+    server.close()    
 if __name__ == "__main__":
     main()
