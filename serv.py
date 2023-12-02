6368e9809cbac8d32d998ecea7f81314d0838766
Marcilino421 <Maromicho12@gmail.com>
2023-12-02 13:20:21 -0800

    reformated the serv.py

--- serv.py
+++ serv.py
@@ -1,8 +1,8 @@
 import socket
-import sys 
+import sys
 import os
 
-# Command line arugment check
+# Command line argument check
 if len(sys.argv) < 2:
     print("USAGE: python serv.py <PORT NUMBER>")
     sys.exit(1)
@@ -10,22 +10,22 @@ if len(sys.argv) < 2:
 listenPort = int(sys.argv[1])
 
 welcomeSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-welcomeSock.bind(('', listenPort)) #Binds to localhost
+welcomeSock.bind(('', listenPort))  # Binds to localhost
 welcomeSock.listen(1)
 
 # Directory where to store files from client
 data_dir = 'data'
-os.makedirs(data_dir, exist_ok=True) # Make sure that the path exists
+os.makedirs(data_dir, exist_ok=True)  # Make sure that the path exists
 
 
 def recvAll(sock, numBytes):
     recvBuff = b""
     tmpBuff = b""
 
-    # Keep receiving till all is received
+    # Keep receiving until all is received
     while len(recvBuff) < numBytes:
         # Attempt to receive all bytes
-        tmpBuff =  sock.recv(numBytes)
+        tmpBuff = sock.recv(numBytes)
 
         # Check to make sure the other side has not closed
         if not tmpBuff:
@@ -34,6 +34,8 @@ def recvAll(sock, numBytes):
         # Add the received bytes to the buffer
         recvBuff += tmpBuff
     return recvBuff
+
+
 # Accept connections forever
 while True:
 
@@ -45,31 +47,55 @@ while True:
     print("Accepted connection from client: ", addr)
     print("\n")
 
-    filename = clientSock.recv(1024).decode()
-    fileData = ""
+    command = clientSock.recv(1024).decode()
 
-    # The temporary buffer to store the received data
-    recvBuff = ""
-    fileSizeBuff = ""
-    
-    fileSize = 0	
+    if command.startswith('get '):
+        # Client wants to download a file
+        filename = command[4:]
 
-    # Receive the first 10 bytes indicating the
-    # size of the file
-    fileSizeBuff = recvAll(clientSock, 10)
-        
-    fileSize = int(fileSizeBuff)
+        filepath = os.path.join(data_dir, filename)
 
-    print("The file size is", fileSize)
+        if os.path.exists(filepath):
+            # File exists, send file to the client
+            fileSize = os.path.getsize(filepath)
+            fileSizeStr = str(fileSize).zfill(10)
 
-    # Get the file data
-    fileData = recvAll(clientSock, fileSize)
+            clientSock.send(fileSizeStr.encode())
 
-    # Write the data to a file in the data directory
-    with open(os.path.join(data_dir, filename), 'wb') as f:
-        f.write(fileData)
+            with open(filepath, 'rb') as f:
+                fileData = f.read()
+                clientSock.sendall(fileData)
 
-    print(f"Received file {filename}")
+            print(f"Sent file {filename} to {addr}")
+
+        else:
+            # File does not exist
+            clientSock.send("FAILURE File does not exist".encode())
+
+    elif command.startswith('put '):
+        # Client wants to upload a file
+        filename = command[4:]
+
+        fileSizeStr = recvAll(clientSock, 10).decode()
+        fileSize = int(fileSizeStr)
+
+        fileData = recvAll(clientSock, fileSize)
+
+        with open(os.path.join(data_dir, filename), 'wb') as f:
+            f.write(fileData)
+
+        print(f"Received file {filename} from {addr}")
+
+    elif command == 'ls':
+        # Client requests file listing
+        file_list = os.listdir(data_dir)
+        file_list_str = "\n".join(file_list)
+        clientSock.sendall(file_list_str.encode())
+        print(f"Sent file list to {addr}")
+
+    else:
+        # Unknown command
+        print('Unknown command')
 
     # Close our side
     clientSock.close()
